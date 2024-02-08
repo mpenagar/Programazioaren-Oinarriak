75dbd4006ca61894eaaaf9bca30363918c05dede
Mikel Penagarikano <mpenagar@yahoo.es>
2024-02-08 16:13:59 +0100

    .

--- .gitUpdate.sh
+++ .gitUpdate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo $0
-cd $(dirname $0)
-
-if [[ `git status --porcelain` ]]; then
-    git add .
-    git commit -m "." 
-    git push origin master
-    echo DONE
+if [[ `whoami` == "mpenagar" ]] ; then
+    cd $(dirname $0)
+    if [[ `git status --porcelain` ]]; then
+        git add .
+        git commit -m "." 
+        git push origin master
+        echo DONE
+    fi
+else
+    su mpenagar $0
 fi
