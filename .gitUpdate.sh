b205ff404d1b33b3b6efb43ebb7cc231d42f3bc7
Mikel Penagarikano <mpenagar@yahoo.es>
2024-02-08 16:40:01 +0100

    autosync

--- .gitUpdate.sh
+++ .gitUpdate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# v1.0 2024/02/08
+
 
 if [[ `whoami` == "mpenagar" ]] ; then
     cd $(dirname $0)
     if [[ `git status --porcelain` ]]; then
         git add .
-        git commit -m "." 
+        git commit -m "autosync" 
         git push origin master
     fi
 else
